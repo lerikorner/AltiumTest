e43ea3fbedf0ba805d18778ef813f35a80873a08
lerikorner <samara@spaceofjoy.ru>
2017-12-20 07:30:30 +0300

    Hello Bali

---
+++ AltiumTest/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace AltiumTest
+{
+    public class KeyGenerator
+    {
+        public static int GetRandomStringLength(int left, int right)
+        {
+            Random rnd = new Random();
+            return rnd.Next(left, right);
+        }
+        public static string GetUniqueKey(int maxSize)
+        {
+            char[] chars = new char[75];
+            chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.!?()@#%^-_ ".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
--- AltiumTest/Program.cs
+++ AltiumTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AltiumTest;
 
 namespace AltiumTest
 {
@@ -10,6 +12,16 @@ namespace AltiumTest
     {
         static void Main(string[] args)
         {
+            string fileName = "c:\\temp\\out2.txt";
+            FileStream aFile = new FileStream(fileName, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(aFile);
+            aFile.Seek(0, SeekOrigin.End);
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                sw.WriteLine(AltiumTest.KeyGenerator.GetUniqueKey(rnd.Next(0,1024)));
+            }
+            sw.Close();
         }
     }
 }
